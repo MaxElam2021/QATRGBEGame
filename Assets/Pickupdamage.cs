6de55bc211a141981a0c65a890603b573f8fb285
MaxElam2021 <94044736+MaxElam2021@users.noreply.github.com>
2022-03-30 10:04:23 +0100

    Pickup and Health works well

--- Assets/Pickupdamage.cs
+++ Assets/Pickupdamage.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Pickupdamage : MonoBehaviour
 {
+    PlayerController player;
+    Health health;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerController>();
+        health = FindObjectOfType<Health>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+           if (other.tag == "Player")
+           {
+            health.TakeDamage(10);
+             
+           }
     }
 }
