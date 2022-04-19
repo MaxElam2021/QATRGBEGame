8018c5d1e7357c268f61216f42763baebf8c569b
MaxElam2021 <94044736+MaxElam2021@users.noreply.github.com>
2022-04-19 14:58:44 +0100

    Added dialgoue code

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
+	[SerializeField] GameObject dialogue;
+	PlayerMovement player;
+
+	bool hasShown = false;
+
+	bool optionOne = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+		dialogue.SetActive(false); 
+		player = FindObjectOfType<PlayerMovement>();
     }
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (hasShown == false) 
+		{
+			if (other.tag == "Player") 
+			{
+				dialogue.SetActive(true);
+				player.DialogEnter();
+				hasShown = true;
+			}
+		}
+	}	
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
