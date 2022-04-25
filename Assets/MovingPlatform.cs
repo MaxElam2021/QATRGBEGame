6aee9d6e31347dcfe778583503b8aab1e98e3c12
MaxElam2021 <94044736+MaxElam2021@users.noreply.github.com>
2022-04-25 10:41:21 +0100

    Moving -platform

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;     //Variable that allows me to speed up said platform itself
+    public int startingPoint;       //starting index (Position of the platform)
+    public Transform[] points;      //Array of transform point
+
+    private int i; 
+    
+    void Start()
+    {
+        transform.position = points[startingPoint].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   // checking the distance of the platform and the point
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        {
+            i++;
+            if (i == points.Length)
+            {
+                i = 0;
+            }
+        }
+        //moving the platform to the point position with the index "i"
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.transform.SetParent(transform);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.transform.SetParent(null);
+    }
+}
