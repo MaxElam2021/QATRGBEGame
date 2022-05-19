497eedd6219e95a4ae04e42818b98c4ec76625af
MaxElam2021 <94044736+MaxElam2021@users.noreply.github.com>
2022-05-19 10:43:11 +0100

    Remodded Main Menu scene

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -7,6 +7,11 @@ public class PauseMenu : MonoBehaviour
 {
     [SerializeField] GameObject pauseMenu;
 
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
     public void Pause()
     {
         pauseMenu.SetActive(true);
